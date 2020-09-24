048dcbc21cbffd60b0953614ad109d1e92eef36d
Elena <Elena@MacBook-Pro-van-Elena.local>
2020-09-24 15:28:13 +0200

    Initial commit

---
+++ week 2/2.14 excercises.py
@@ -0,0 +1,25 @@
+Var1 = "all"
+Var2 = "work"
+Var3 = "and"
+Var4 = "no"
+Var5 = "play"
+Var6 = "makes"
+Var7 = "jack"
+Var8 = "a"
+Var9 = "dull"
+Var10 = "boy"
+print(Var1,Var2, Var3, Var4, Var5, Var6,Var7, Var8, Var9,Var10)
+print(6 * (1 - 2))
+
+bruce = 6
+print(bruce + 4)
+
+print (5 % 2)
+print (9 % 5)
+print (15 % 12)
+print (12 % 15)
+print (6 % 6)
+
+print(51/24)
+print(0.125*60)
+print(14+7.5)
